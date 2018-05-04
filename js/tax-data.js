877bedede46cdb99eac50f56dc9c07aca82f4c7b
marina_sakovich <sakovichm@mail.ru>
2018-05-04 15:03:25 +0300

    add promise but it is commit

--- js/tax-data.js
+++ js/tax-data.js
@@ -18,9 +18,9 @@
         }
 
         request() {
-            let xhr = new XMLHttpRequest();
-
             let url = `${firstPartUrl}${this.currency}?onDate=${this.date}&${lastPartUrl}`;
+
+            let xhr = new XMLHttpRequest();
             xhr.open('GET', url, true);
 
             let allSum = 0;
@@ -28,7 +28,7 @@
 
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
-                    // console.log(xhr.responseText);
+                     console.log(xhr.responseText);
 
                     let reply = JSON.parse(xhr.responseText);
 
@@ -40,7 +40,48 @@
             }
 
             xhr.send();
+
+            // this.httpGet(url)
+            //     .then(
+            //         (response) => {
+            //             console.log(response);
+            //             let reply = JSON.parse(response);
+            //             let allSum = (this.income * reply.Cur_OfficialRate / reply.Cur_Scale).toFixed(2);
+            //             this.tax = (allSum / 100 * this.percent).toFixed(2);
+            //
+            //             alert(`Fulfilled: ${this.tax}`);
+            //         },
+            //         (error) => {
+            //             alert(`Rejected: ${error}`);
+            //         }
+            //     );
         }
+
+        // httpGet(url) {
+        //
+        //     return new Promise(function(resolve, reject) {
+        //
+        //         let xhr = new XMLHttpRequest();
+        //         xhr.open('GET', url, true);
+        //
+        //         xhr.onreadystatechange = function() {
+        //             if (this.readyState === 4) {
+        //                 console.log(this.responseText);
+        //                 resolve(this.responseText);
+        //             } else {
+        //                 let error = new Error(this.statusText);
+        //                 error.code = this.status;
+        //                 reject(error);
+        //             }
+        //         };
+        //
+        //         xhr.onerror = function() {
+        //             reject(new Error("Network Error"));
+        //         };
+        //
+        //         xhr.send();
+        //     });
+        // }
     }
 
     window.TaxData = TaxData;
